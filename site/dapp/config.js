0b7ed6db4ad097b90f04fc9fc61622e4a06b41c5
root <real0rouse@gmail.com>
2024-06-16 17:55:23 +0200

    fixed getTimestampOfNextWithdrawalHalving in contract to show timestamp of withdrawalHalving even when Global Lock Time is active

--- site/dapp/config.js
+++ site/dapp/config.js
@@ -1,6 +1,6 @@
 
 const contract1Address = '0x19E6BF254aBf5ABC925ad72d32bac44C6c03d3a4'; // Replace with actual Contract 1 address
-const contract2Address = '0x48f6B9B82d72f886e9694132759e78FD07844EEA'; // Replace with actual Contract 2 address
+const contract2Address = '0xFf9C40740747fAe62A2Af5fe99B31C12C7853967'; // Replace with actual Contract 2 address
 const tokenContractAddress = '0x70dE1F953A44D8B0862746F1cfD1041c2331e0ab';
 // DEPRECATED - const rewardsReserveContractAddress = '0x948b5A9C7BefcF6Fe336278B968c8Db442a34ed0';
 
