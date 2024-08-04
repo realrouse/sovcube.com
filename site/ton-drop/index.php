b34e39e6ee4989492eec55ce78f928f168abc518
root <real0rouse@gmail.com>
2024-08-04 10:53:37 +0200

    modify ton-drop

--- site/ton-drop/index.php
+++ site/ton-drop/index.php
@@ -20,11 +20,11 @@
         <div class="blurred-background"></div>
 
         <div class="text-container">
-            <h1 class="brand-name">SOVCUBE-DROP</h1>
+            <h1 class="brand-name">BSOV-DROP</h1>
             <h2>Buy the token on TON chain, and claim BSOV Tokens when SovCube is released.</h2>
 
-	    <p><b>Step 1.</b> Buy SOVCUBE-DROP</p>
-	<p><b>Step 2.</b> Send your SOVCUBE-DROP to the burn address at: EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c<br>
+	    <p><b>Step 1.</b> Buy <a href="https://dedust.io/pools/EQBaKVmG9lKoU6pwu4oGac9nrdvCmaWMsBOt3h7V7bPPYuBo">BSOV-DROP</a></p>
+	<p><b>Step 2.</b> Send your BSOV-DROP to the burn address at: EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c<br>
 and IMPORTANT: Write your ERC20/ETH address in the memo.</p>
 	<p><b>Step 3.</b> Wait until you receive BSOV Tokens, and join the SovCube Telegram.</p>
 
