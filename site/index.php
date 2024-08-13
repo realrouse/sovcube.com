58993db13865841283d2bbb9c821acac95b9e3a5
root <real0rouse@gmail.com>
2024-08-13 16:47:41 +0200

    rewriting landing page

--- site/index.php
+++ site/index.php
@@ -42,35 +42,65 @@
 <center><h1 class="brand-name">SovCube</h1></center>
 <center><img  src="/images/ani-locked-bsov-padlock-small.gif" style="margin:0px 0px 0px 0px; z-index:11;" width="100px" alt="BSOV Token" ></img></center><br>
 <center><h2 class="tagline">Timelock Today. Shape Tomorrow.</h2></center>
-<p style="text-align:center;">Unlock the potential of your BSOV Tokens with SovCube, a web3 dApp built on Ethereum.
-<br>Timelock, earn rewards, and influence real-world outcomes in our decentralized foundation.</p>
-<center><h3 class="description-heading">Great decisions take time. Shape the future with your vote in our decentralized foundation.</h3></center>
+<p style="text-align:center;">Unlock the potential of your <a href="https://bsovtoken.com" target="_blank">BSOV Tokens</a> by locking them using the SovCube dApp, a decentralized web3 app built on the Ethereum blockchain. Timelock, earn rewards, and join a decentralized foundation where the future is whatever the community decides.</p>
+<center><h3 class="description-heading">Great decisions take time.<br>Shape the future with thoughtful decisions and your vote in our decentralized foundation.</h3></center>
 <button onclick="window.location.href='#more'" class="launch-button-gold">Read More</button>
 </div>
 
     <div class="text-container">
 
 <center><h2 class="tagline" id="more">Long Commitment, Lasting Impact</h2></center>
-<center><h3 class="description-heading">SovCube is a decentralized autonomous organization (DAO) that allocates donated Treasury funds to various causes. Any member can become a decision maker by demonstrating their commitment through locking their <a href="https://bsovtoken.com" target="_blank">BSOV Tokens</a> for an extended period.</h3></center>
+<center><h3 class="description-heading">Join SovCube, a decentralized autonomous organization (DAO) where your commitment directs Treasury funds to meaningful causes.<br><br>Any member can qualify to become a decision maker by demonstrating their commitment through locking their <a href="https://bsovtoken.com" target="_blank">BSOV Tokens</a> for an extended period.</h3></center>
 <p style="text-align:center;"></p>
 <!-- <p style="text-align:center;">Secure your <a href="https://bsovtoken.com" target="_blank">BSOV Tokens</a> with SovCube's web3 dApp<br>and smart-contract collection, which work similarly to a traditional long-term savings account.</p>-->
 <br>
 <center><h2 class="tagline">Features</h2></center>
 <div class="stats-container">
 <center><h3>Timelock Tokens</h3></center>
-<div style="text-align:center;">
-<p>Timelock your <a href="https://bsovtoken.com" target="_blank">BSOV Tokens</a> using SovCube's web3 interface to showcase your commitment to the token's long-term value. Become a Top Timelocker to qualify for voting power and earn voting rewards.</p>
-<img src="/images/timelock-gold-padlock5.png" style="margin:0px 0px 0px 0px; z-index:11;" width="300px" ></img><br>
+<div style="text-align:left;">
+<p>Timelock your <a href="https://bsovtoken.com" target="_blank">BSOV Tokens</a> using SovCube's web3 interface, which means locking them in a secure smart contract for a set period. During this time, your tokens cannot be moved or traded, demonstrating your commitment to the token's long-term value.</p>
+<p> As a reward for your dedication, you can become a Top Timelocker, qualifying for voting power and earning additional rewards.</p>
+
+<details>
+   <summary> <center><h4>Timelock Periods</h4></center></summary>
+    <ul>
+        <li>
+            <p><strong>Onboarding Period:</strong> For the first 1000 days after contract deployment, all timelocked tokens are locked for the entire period.</p>
+        </li>
+        <li>
+            <p><strong>Post-Onboarding:</strong> After the 1000-day period, new users face a 70-day lock, while existing users only have a 14-day lock.</p>
+        </li>
+    </ul>
+
 <!--<img src="/images/Sovcube-padlock-icon10.png" style="margin:0px 0px 0px 0px; z-index:11;" width="100px" ></img><br>-->
 </div>
+</details>
+<center><img src="/images/timelock-gold-padlock5.png" style="margin:0px 0px 0px 0px; z-index:11;" width="300px" ></img></center><br>
 </div>
 
 <div class="stats-container">
-<div style="text-align:center;">
-<center><h3 style="color:#F8B128;">Timelock Rewards for early users</h3></center>
-<p>Earn BSOV rewards by timelocking, and potentially<br>double your investment (100% ROI) when you're an early participant of SovCube's Timelock Rewards.</p>
+<div style="text-align:left;">
+<center><h3 style="color:#F8B128;">Timelock Rewards for Early Participants</h3></center>
+<p>During SovCube's initial onboarding phase, the SovCube smart-contract offers an exclusive opportunity to maximize your BSOV token holdings. By participating early in the Timelock Rewards System, you can earn substantial rewards (up to 100% ROI) as the community progresses through various tiers.
 <br>
+<div class="how-it-works">
+<details>
+   <summary> <center><h4>How It Works:</h2></center></summary>
+    <ul>
+        <li> <p>         <strong>Earn up to 100% ROI:</strong> The sooner you timelock your BSOV Tokens, the higher your potential return on investment (ROI). Early participants can earn up to 100% ROI in the first reward tier.
+       </p> </li>
+        <li><p>
+            <strong>Progress Through Reward Tiers:</strong> As more tokens are timelocked by the community, the reward tiers decrease, starting from 150,000 BSOV in Tier 1, with each subsequent tier offering reduced rewards.
+       </p> </li>
+        <li><p>
+            <strong>Limited-Time Offer:</strong> This opportunity is only available during the initial onboarding phase, so the earlier you participate, the greater your rewards.
+     </p>   </li>
+    </ul>
+
 </div>
+
+</div>
+</details>
 <div style="text-align:center;">
 <!--<img src="/images/Sovcube-padlock-icon11.png" style="margin:0px 0px 20px 0px; z-index:11;" width="200px" ></img>-->
 <img src="/images/sovcube-reward2.png" style="margin:0px 0px 20px 0px; z-index:11;" width="55px" ></img>
@@ -78,25 +108,50 @@
 </div>
 </div>
 <div class="stats-container">
-<div style="text-align:center;">
+<div style="text-align:left;">
 <center><h3>Send or Pay using Timelocked Tokens</h3></center>
 
-<p>Once you've timelocked your tokens, you can still use them!<br>
- You can offer individuals a payment that remains locked for 100 days.<br> 
-In essence, you can send, gift, or pay anyone with timelocked BSOV Tokens.</p>
+<p>Your timelocked tokens aren't just locked away—they're flexible. Use them to make payments, send gifts, or offer donations, all while ensuring they remain locked for 100 days in the address of the receiver.</p>
 </p>
-<img src="/images/send-locked-tokens-users8.png" style="margin:0px 0px 20px 0px; z-index:11;" width="400px" ></img>
+<center><img src="/images/send-locked-tokens-users8.png" style="margin:0px 0px 20px 0px; z-index:11;" width="400px" ></img></center>
 </div>
 <br>
 </div>
 
+
 <div class="stats-container">
-<div style="text-align:center;">
+    <div style="text-align:left;">
+        <center><h3>Withdrawal Process</h3></center>
+        <p>The Gradual Release Withdrawal Mechanism (GRWM) is designed to ensure market stability by controlling the flow of token withdrawals, preventing any sudden large-scale sell-offs.</p>
+        <br>
+        <div class="how-it-works">
+	 
+<details>
+ <summary>  <center><h4>Gradual Release Withdrawal Mechanism:</h4></center></summary>
+            <ul>
+                <li>
+                    <p><strong>Weekly Limit:</strong> Withdrawals are capped at 100 BSOV tokens per week per user, allowing for a steady and controlled release of tokens.</p>
+                </li>
+                <li>
+                    <p><strong>Withdrawal Halvings:</strong> Every 4 years, the weekly withdrawal limit is halved. For example, after the first halving, the limit will be reduced to 50 BSOV tokens per week, further slowing the release rate over time.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</details>
+    <div style="text-align:center;">
+        <!--<img src="/images/Sovcube-padlock-icon11.png" style="margin:0px 0px 20px 0px; z-index:11;" width="200px" ></img>-->
+        <!--<img src="/images/sovcube-reward2.png" style="margin:0px 0px 20px 0px; z-index:11;" width="55px"></img> -->
+    </div>
+</div>
+
+
+
+<div class="stats-container">
+<div style="text-align:left;">
 <center><h3>Propose, Vote, Be Rewarded (Under development)</h3></center>
-<p>The decentralized foundation spends donated funds on important causes. Which kind of causes? It can be anything from charities to funding advertisement to promote this project. It is completely up to the Proposal Makers and the Top Timelockers.
-Only the Top Timelockers can vote and receive Voting Rewards, but anyone can write proposals.
-Will you take part in shaping the future?</p>
-</p>
+<p>The decentralized foundation allocates funds to significant causes. What kind of causes? It’s completely up to the community. Proposal Makers suggest ideas, and Top Timelockers vote to decide how the funds are spent, ensuring that the direction of SovCube reflects the priorities of its most committed members.</p>
+    <p>Only the Top Timelockers can vote and receive Voting Rewards. However, anyone in the community is welcome to write and submit proposals, giving every member a voice in shaping the future.</p>
 </div>
 <br>
 </div>
@@ -106,10 +161,10 @@ Will you take part in shaping the future?</p>
 </div>
 
 <div class="button-container">
-	<button onclick="window.location.href='/dapp/index.php'" class="launch-button">Launch dApp</button>
-<p id="message" style="display: none; color: red; margin-top: 10px;">Under construction</p>
+    <button onclick="window.location.href='/dapp'" class="launch-button">Launch dApp</button>
+    <button onclick="window.location.href='/docs'" class="launch-button">Read the Docs & Help</button>
+    <p id="message" style="display: none; color: red; margin-top: 10px;">Under construction</p>
 </div>
-
 
 </div>
 </div>
