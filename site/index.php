25eb042703902dc05988189fae53748ca2137278
root <real0rouse@gmail.com>
2024-10-01 08:19:22 +0200

    mobile view adjustments

--- site/index.php
+++ site/index.php
@@ -9,7 +9,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title>SovCube</title>
 <link rel="stylesheet" href="styles.css">
 <link rel="stylesheet" href="styles-fonts.css">
