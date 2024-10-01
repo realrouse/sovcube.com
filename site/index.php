7b1136c16fb0c7b8b8cded3d95b8ed231041563b
root <real0rouse@gmail.com>
2024-10-01 08:50:12 +0200

    mobile adjustment

--- site/index.php
+++ site/index.php
@@ -73,7 +73,7 @@ if ($contract2Data) {
 <center><h1 class="brand-name">SovCube</h1></center>
 <center><img  src="/images/ani-locked-bsov-padlock-small.gif" style="margin:0px 0px 0px 0px; z-index:11;" width="100px" alt="BSOV Token" ></img></center><br>
 <center><h2 class="tagline">Timelock Today. Shape Tomorrow.</h2></center>
-<p style="text-align:center;" class="tagline-body">Unlock the potential of your BSOV Tokens, a cryptocurrency designed as a Store-of-Value (SoV), by locking them using the SovCube dApp, a decentralized web3 app built on the Ethereum blockchain. By timelocking your BSOV Tokens, you can earn rewards and participate in a decentralized foundation where the community collectively decides the future.</p>
+<p style="text-align:center;" class="tagline-body">Unlock the potential of your BSOV Tokens, a Store-of-Value (SoV) cryptocurrency, by locking them in the SovCube dApp, built on Ethereum. By timelocking your tokens, you can earn rewards and participate in a community-driven decentralized foundation.</p>
 <center><h3 class="description-heading">Great decisions take time.<br>Shape the future with thoughtful decisions and your vote in our decentralized foundation.</h3></center>
 <button onclick="window.location.href='#more'" class="launch-button-gold">Read More</button>
 <div class="arrow-down"></div>
